f85b284a25ef9e4bee5860723cf6f254dfc0ea0c
karypzhanov-k-auca-2022 <112881593+karypzhanov-k-auca-2022@users.noreply.github.com>
2022-09-07 14:55:56 +0600

    Update Problem01.java

--- lab-01/src/Problem01.java
+++ lab-01/src/Problem01.java
@@ -3,10 +3,5 @@ import java.util.Scanner;
 public class Problem01 {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("What is your name?");
-        System.out.println("My name is:");
-        Scanner scanner = new Scanner(System.in);
-        String myName = scanner.next();
-        System.out.println("Nice to meet you, " + myName);
-    }
+           }
 }
