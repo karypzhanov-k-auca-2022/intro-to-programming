f9bdd71046b5d18484a0bb4626bc1782fa303506
Kairat Karypzhanov <kairat_k@auca.kg>
2022-09-12 19:37:04 +0600

    Problem03 is completed

--- lab-01/src/Problem03.java
+++ lab-01/src/Problem03.java
@@ -4,6 +4,5 @@ public class Problem03 {
         System.out.println("     J     A A    V   V  A A");
         System.out.println(" J   J    AAAAA    V V  AAAAA");
         System.out.println("   JJ    A     A    V  A     A");
-
     }
 }
