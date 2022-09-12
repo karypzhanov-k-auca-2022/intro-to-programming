6d542104ccbd378fd11124b4f96df672f74bd8de
Kairat Karypzhanov <kairat_k@auca.kg>
2022-09-12 20:24:53 +0600

    completed

---
+++ lab-01/src/Problem05.java
@@ -0,0 +1,17 @@
+public class Problem05 {
+    public static void main(String[] args) {
+        double a = 5.1;
+        double b = 2.6;
+        double plusAb = a + b;
+        double minesAb = a - b;
+        double multiplyAb = a * b;
+        double divideAb = a / b;
+        double residueAb = a % b;
+        System.out.println(a + " + " + b + " = "+ plusAb);
+        System.out.println(a + " + " + b + " = "+ minesAb);
+        System.out.println(a + " + " + b + " = "+ multiplyAb);
+        System.out.println(a + " + " + b + " = "+ divideAb);
+        System.out.println(a + " + " + b + " = "+ residueAb);
+
+    }
+}
