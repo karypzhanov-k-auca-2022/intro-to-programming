89c5c5943879979d54c6f35bd60ca06216932a76
Kairat Karypzhanov <kairat_k@auca.kg>
2022-09-12 19:48:30 +0600

    Problem06 is completed

--- lab-01/src/Problem03.java
+++ lab-01/src/Problem03.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Problem04 {
+public class Problem03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What is your name?");
