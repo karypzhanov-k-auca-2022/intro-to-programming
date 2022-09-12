8131be1ab69daa13e185917ace8385c6b77cec2a
Kairat Karypzhanov <kairat_k@auca.kg>
2022-09-12 15:21:38 +0600

    Problem02 almost completed

--- lab-01/src/Problem01.java
+++ lab-01/src/Problem01.java
@@ -1,5 +1,8 @@
 public class Problem01 {
     public static void main(String[] args) {
         System.out.println("Hello world");
-                    }
+        System.out.println("*******************+**");
+        System.out.println("* I like programming *");
+        System.out.println("*******************+**");
+    }
 }
---
+++ lab-01/src/Problem02.java
@@ -0,0 +1,12 @@
+public class Problem02 {
+    public static void main(String[] args) {
+        System.out.println("********************");
+        System.out.println("*I like programming*");
+        System.out.println("********************");
+
+        System.out.println("     J      A    V     V  A");
+        System.out.println("     J     A A    V   V  A A");
+        System.out.println(" J   J    A   A    V V  A   A");
+        System.out.println("   JJ    A     A    V  A     A");
+    }
+}
