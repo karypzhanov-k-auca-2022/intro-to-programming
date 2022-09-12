861a25f7394bd2b4c36684c622ac9c75ea3bce76
Kairat Karypzhanov <kairat_k@auca.kg>
2022-09-12 19:38:26 +0600

    Problem02 is completed

--- lab-01/src/Problem02.java
+++ lab-01/src/Problem02.java
@@ -3,10 +3,5 @@ public class Problem02 {
         System.out.println("********************");
         System.out.println("*I like programming*");
         System.out.println("********************");
-
-        System.out.println("     J      A    V     V  A");
-        System.out.println("     J     A A    V   V  A A");
-        System.out.println(" J   J    AAAAA    V V  AAAAA");
-        System.out.println("   JJ    A     A    V  A     A");
     }
 }
