6d314cdcd32eed351352b426082a2e27a5094587
Kairat Karypzhanov <kairat_k@auca.kg>
2022-10-04 19:27:36 +0600

    Finish Problems 01-05 in oj-midterm

---
+++ oj-midterm/src/Problem03.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int y = scanner.nextInt();
+        int m = scanner.nextInt();
+        System.out.println(2 * m - y);
+    }
+}
---
+++ oj-midterm/src/Problem05.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Problem05 {
+    public static void main(String[] args) {
+        int students = 28;
+        Scanner scanner = new Scanner(System.in);
+        int bigBox = scanner.nextInt();
+        int littleBox = scanner.nextInt();
+        int cakes = bigBox * 8 + littleBox * 3 - students;
+        System.out.println(cakes);
+    }
+}
