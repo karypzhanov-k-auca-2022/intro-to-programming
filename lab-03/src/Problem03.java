ca0d58b05a5fd8ff2fa04044c14fef8554c9d025
Kairat Karypzhanov <kairat_k@auca.kg>
2022-10-24 23:16:24 +0600

    Finish Problems 3,4,6 and 7 in Lab-03

--- lab-03/src/Problem03.java
+++ lab-03/src/Problem03.java
@@ -1,6 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+
 public class Problem03 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
--- lab-03/src/Problem07.java
+++ lab-03/src/Problem07.java
@@ -1,6 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+
 public class Problem07 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
